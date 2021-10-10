5e8d3937f06f5ebc6ef1ba13e5d0d3dac38fa53e
Znenyapechenya <epsareva77@gmail.com>
2021-10-11 02:34:19 +0700

    hey

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = 'Jane';
+let flag = true;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
+        <p style = {{
+          color: 'yellow',
+          fontSize: 46
+        }}>
+          Hello, my name is {name}
         </p>
+        <p style = {{
+          color: 'yellow'
+        }}>
+          I have {2} apples. My friend has {2+2} apples. {flag && 'Flag is true'}
+          {flag ? <p>We are happy</p> : <p>We are unhappy</p>}
+        </p>
+        <div>{undefined}</div>
+        <div>{null}</div>
+        <div>{false}</div>
+        <div>{true}</div>
         <a
           className="App-link"
           href="https://reactjs.org"
